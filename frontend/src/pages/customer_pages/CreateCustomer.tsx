a8299dacdce74ae0ab46efff528bcd85565f55de
Lou Diamond Loyloy <loudiamond_loyloy@insideranken.org>
2024-03-21 12:37:02 -0500

    Update CreateCustomer.tsx

--- frontend/src/pages/customer_pages/CreateCustomer.tsx
+++ frontend/src/pages/customer_pages/CreateCustomer.tsx
@@ -1,19 +1,23 @@
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import ManageCustomerForm from "../../forms/CustomerForm/CustomerForm";
 import { useAppContext } from "../../contexts/AppContext";
 import * as apiClient from '../../api-client';
+import { useNavigate } from "react-router-dom";
 
 
 const CreateCustomer = () => 
 {
-  
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const {showToast} = useAppContext();
 
   const {mutate, isLoading} = useMutation(apiClient.createCustomer, 
     {
       onSuccess: async () =>
       {
+        queryClient.invalidateQueries("validateToken");
         showToast({message: 'Successfully Registered.', type:"success"});
+        navigate('/')
       },
       onError: (error: Error)=>
       {
